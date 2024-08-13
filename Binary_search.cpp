51a289207271bcb0557178f2ca8e8583bf70aac5
Rohan V Anvekar <105410447+Anvekar123@users.noreply.github.com>
2024-08-13 23:25:35 +0530

    Update Binary_search.cpp

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -562,3 +562,21 @@ while(low<=high)
 }
 return low;
 }
+int missingK(vector < int > arr, int n, int k) {
+int low=0, high = arr.size()-1;
+while(low<=high)
+{
+    int mid = (low+high)/2;
+    int missing  = arr[mid] - (mid+1);
+    if(missing < k)
+    {
+        low=mid+1;
+    }
+    else
+    {
+        high= mid-1;
+    }
+}
+
+return k+1+high;
+}
